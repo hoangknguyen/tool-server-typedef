230a27cadf545e2aebe6726ff2c46a6efc2b1994
Hoang Nguyen <hoangknguyen@kms-technology.com>
2019-05-22 15:05:34 +0700

    REACT-9 #comment Releasing new version

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from './models/AccountStatus';
 export * from './models/SyncAccountType';
 export * from './models/TransactionType';
 export * from './models/MessageType';
+export * from './models/Snippet';
 export * from './Buxfer';
 export * from './API';

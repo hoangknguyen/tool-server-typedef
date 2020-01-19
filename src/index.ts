44dc56d70228d222837b5eb78972c0df42bb3e1f
Hoang Nguyen <atlassian.nkhoang.it@gmail.com>
2020-01-19 13:11:28 +0700

    REACT-52 #comment Adding note about the exporting the nested index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,5 @@ export * from './models/User';
 export * from './buxfer/buxfer.namespace';
 export * from './buxfer/api.model';
 export * from './buxfer/buxfer.schema'
+// NOTE: DO NOT USE export * from './buxfer/' => not sure why export nested index not working
 export * from './API';

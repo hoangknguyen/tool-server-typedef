db2bd0c06cd77272ed2e16dc548aeb1bde8da05b
Hoang Nguyen <atlassian.nkhoang.it@gmail.com>
2020-05-13 13:47:01 +0700

    REACT-51 #comment Moving BuxferCredential to typedef, updated UserConfiguration type

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,7 @@
-import { ReportingGroup } from '../buxfer';
+import { BuxferCredential, ReportingGroup } from '../buxfer';
 
 export type UserConfiguration = {
     userId: string,
     reportingGroups: ReportingGroup[];
+    buxfer?: BuxferCredential;
 };
